e3a7129dfe7680aac197dd13af7b33d2d1c9fffd
dankar12 <143622228+dankar12@users.noreply.github.com>
2023-09-06 08:44:51 -0600

    Add files via upload

---
+++ ListaTareas1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listatareas1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListaTareas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1 = " Hacer tarea";
+        String tarea2 = " Lavar el coche";
+        String tarea3 = " Lavar la cocina";
+        String tarea4 = " Hacer la comida";
+        String tarea5 = " recoger el cuarto";
+        
+        System.out.println("Hola mi nombre es Daniel y mi padre me asigno hacer todas estas tareas;" + tarea1 + tarea2 + tarea3 + tarea4 + tarea5);
+    }
+   
+    
+}
